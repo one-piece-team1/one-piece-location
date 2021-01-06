d1d106b1f4ca40ea9bb62a9a18ec384667158c18
李韻晟 <dreak603@gmail.com>
2021-01-07 03:03:27 +0800

    feat: adding location repository layer and remove logging from db

--- src/locations/location.module.ts
+++ src/locations/location.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationController } from './location.controller';
+import { LocationRepository } from './location.repository';
 import { LocationService } from './location.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([LocationRepository])],
   controllers: [LocationController],
   providers: [LocationService],
 })
---
+++ src/locations/location.repository.ts
@@ -0,0 +1,44 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import {
+  EntityManager,
+  EntityRepository,
+  getManager,
+  Repository,
+} from 'typeorm';
+import { CreateLocationDto } from './dto';
+import { Location } from './location.entity';
+
+@EntityRepository(Location)
+export class LocationRepository extends Repository<Location> {
+  private readonly repoManager: EntityManager = getManager();
+  private readonly logger: Logger = new Logger('LocationRepository');
+
+  async createLocation(
+    createLocationDto: CreateLocationDto,
+  ): Promise<Location> {
+    const { locationName, lat, lon, type, country } = createLocationDto;
+    const location = new Location();
+    location.locationName = locationName;
+    location.lat = lat;
+    location.lon = lon;
+    location.type = type;
+    if (country) location.country = country;
+    try {
+      await location.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // throw 409 error when duplicate username
+        throw new ConflictException(
+          `LocationName: ${locationName} already exists`,
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+    return location;
+  }
+}
