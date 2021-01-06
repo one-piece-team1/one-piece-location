0296b5aec64a33b11317d639394c6485d86e477e
李韻晟 <dreak603@gmail.com>
2021-01-07 00:48:08 +0800

    chore: code formatting

--- src/libs/loaddbf.ts
+++ src/libs/loaddbf.ts
@@ -1,16 +1,19 @@
 import { join } from 'path';
 import { DBFFile } from 'dbffile';
-import * as fs from 'fs'
+import * as fs from 'fs';
 
 class dbfHandler {
-  private readonly port_file_path: string = join(process.cwd(), 'datasets/ports/WPI.dbf');
-  
+  private readonly port_file_path: string = join(
+    process.cwd(),
+    'datasets/ports/WPI.dbf',
+  );
+
   public async readPort() {
-    let dbf = await DBFFile.open(this.port_file_path);
+    const dbf = await DBFFile.open(this.port_file_path);
     console.log(`DBF file contains ${dbf.recordCount} records.`);
     console.log(`Field names: ${dbf.fields.map(f => f.name).join(', ')}`);
-    let records = await dbf.readRecords(1);
-    for (let record of records) {
+    const records = await dbf.readRecords(1);
+    for (const record of records) {
       console.log(records);
     }
   }
