7bce30e380fae8eb356286427460a050a58fa4d1
李韻晟 <dreak603@gmail.com>
2021-01-07 23:52:04 +0800

    feat: adding sea turn point migrate script

--- src/libs/loaddbf.ts
+++ src/libs/loaddbf.ts
@@ -1,43 +1,29 @@
 import { join } from 'path';
 import { DBFFile } from 'dbffile';
-import { createConnection, getManager } from 'typeorm';
+import { createConnection } from 'typeorm';
 import { config } from '../../config';
 import { Logger } from '@nestjs/common';
 import * as ELocation from '../locations/enums';
+import * as JSONData from '../../datasets/routes/routes.json';
 import { Location } from '../locations/location.entity';
+import { Turn } from '../turns/turn.entity';
+
+interface IRoues {
+  [key: string]: any;
+}
 
 class DBFHandler {
   private readonly logger: Logger = new Logger('dbfHandler');
   private readonly portFilePath: string = join(process.cwd(), 'datasets/ports/WPI.dbf');
 
-  constructor() {
-    this.init();
-  }
-
-  /**
-   * @description Init Connection
-   */
-  async init() {
-    await createConnection({
-      type: 'postgres',
-      host: config.DB_SETTINGS.host,
-      port: config.DB_SETTINGS.port,
-      username: config.DB_SETTINGS.username,
-      password: config.DB_SETTINGS.password,
-      database: config.DB_SETTINGS.database,
-      entities: [Location],
-      synchronize: true,
-    });
-  }
-
   /**
    * @description Read Port DBF Data and Save Data to Port Table
+   * @returns {Promise<void>}
    */
   public async generatePortData(): Promise<void> {
     const dbf = await DBFFile.open(this.portFilePath);
     const records = await dbf.readRecords();
     for (const record of records) {
-      this.logger.log(JSON.stringify(record), 'ReadPort');
       const locationName: string = record['PORT_NAME'] as string;
       const lat: number = record['LATITUDE'] as number;
       const lon: number = record['LONGITUDE'] as number;
@@ -63,10 +49,56 @@ class DBFHandler {
         .catch((err) => this.logger.log(err.message, 'Create Seed Port Data Fail'));
     }
   }
+
+  /**
+   * @description Read Sea Routes GeoJson Data and Save Data to Turn Table
+   * @returns {Promise<void>}
+   */
+  public async generateRoutesData(): Promise<void> {
+    const geoJSONData = JSONData as IRoues;
+    const geoProperties = geoJSONData.default.features;
+    for (let i = 0; i < geoProperties.length; i++) {
+      console.log('geoProperties: ', geoProperties[i]['geometry']);
+      const coordinates: number[][] = geoProperties[i]['geometry'].coordinates as number[][];
+      const turn = new Turn();
+      turn.lineString = {
+        type: 'LineString',
+        coordinates,
+      };
+      turn.lineStringSrid = {
+        type: 'LineString',
+        coordinates,
+      };
+      turn
+        .save()
+        .then((res) => this.logger.log(JSON.stringify(res), 'Create Seed Routes Data Success'))
+        .catch((err) => this.logger.log(err.message, 'Create Seed Routes Data Fail'));
+    }
+  }
+
+  /**
+   * @description Main Generate Func start with connecting then processing write port and turn operation
+   * @returns {Promise<void>}
+   */
+  async generate() {
+    await createConnection({
+      type: 'postgres',
+      host: config.DB_SETTINGS.host,
+      port: config.DB_SETTINGS.port,
+      username: config.DB_SETTINGS.username,
+      password: config.DB_SETTINGS.password,
+      database: config.DB_SETTINGS.database,
+      entities: [Location, Turn],
+      synchronize: true,
+    }).catch((err) => this.logger.log(err.message, 'Init'));
+
+    this.generatePortData();
+    this.generateRoutesData();
+  }
 }
 
 function bootstrap() {
-  return new DBFHandler().generatePortData();
+  return new DBFHandler().generate();
 }
 
 bootstrap();
